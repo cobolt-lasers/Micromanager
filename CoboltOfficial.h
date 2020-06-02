da9a1013d2e1f16e5b66299b4d73677196cf03db
Lukas Kalinski <lukkal@lasers.int>
2020-06-02 02:25:41 +0200

    MIT license stamps added

--- CoboltOfficial.h
+++ CoboltOfficial.h
@@ -1,12 +1,35 @@
-/**
- * \file        CoboltOfficial.h
- *
- * \brief       Official device adapter for COBOLT lasers.
- *
- * \authors     Johan Crone, Lukas Kalinski
- *
- * \copyright   Cobolt AB, 2020. All rights reserved. // TODO: Set proper license
- */
+///////////////////////////////////////////////////////////////////////////////
+// FILE:       CoboltOfficial.h
+// PROJECT:    MicroManager
+// SUBSYSTEM:  DeviceAdapters
+//-----------------------------------------------------------------------------
+// DESCRIPTION:
+// Cobolt Lasers Controller Adapter
+//
+// COPYRIGHT:     Cobolt AB, Stockholm, 2020
+//                All rights reserved
+//
+// LICENSE:       MIT
+//                Permission is hereby granted, free of charge, to any person obtaining a
+//                copy of this software and associated documentation files( the "Software" ),
+//                to deal in the Software without restriction, including without limitation the
+//                rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+//                sell copies of the Software, and to permit persons to whom the Software is
+//                furnished to do so, subject to the following conditions:
+//                
+//                The above copyright notice and this permission notice shall be included in all
+//                copies or substantial portions of the Software.
+//
+//                THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+//                INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+//                PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+//                HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+//                OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+//                SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+// AUTHORS:
+//                Johan Crone (2020)
+//                Lukas Kalinski / lukas.kalinski@coboltlasers.com (2020)
 
 #ifndef __COBOLT_OFFICIAL_H
 #define __COBOLT_OFFICIAL_H
--- Logger.h
+++ Logger.h
@@ -1,10 +1,34 @@
-/**
- * \file        Logger.h
- *
- * \authors     Lukas Kalinski
- *
- * \copyright   Cobolt AB, 2020. All rights reserved. // TODO: Set proper license
- */
+///////////////////////////////////////////////////////////////////////////////
+// FILE:       Logger.h
+// PROJECT:    MicroManager
+// SUBSYSTEM:  DeviceAdapters
+//-----------------------------------------------------------------------------
+// DESCRIPTION:
+// Cobolt Lasers Controller Adapter
+//
+// COPYRIGHT:     Cobolt AB, Stockholm, 2020
+//                All rights reserved
+//
+// LICENSE:       MIT
+//                Permission is hereby granted, free of charge, to any person obtaining a
+//                copy of this software and associated documentation files( the "Software" ),
+//                to deal in the Software without restriction, including without limitation the
+//                rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+//                sell copies of the Software, and to permit persons to whom the Software is
+//                furnished to do so, subject to the following conditions:
+//                
+//                The above copyright notice and this permission notice shall be included in all
+//                copies or substantial portions of the Software.
+//
+//                THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+//                INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+//                PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+//                HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+//                OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+//                SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+// AUTHORS:
+//                Lukas Kalinski / lukas.kalinski@coboltlasers.com (2020)
 
 #ifndef __COBOLT__LOGGER
 #define __COBOLT__LOGGER
